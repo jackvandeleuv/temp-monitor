a04068ed2f424ac9f417cf0f120b3bf2012821da
Jack Vandeleuv <JVandeleuv@clevelandohio.gov>
2025-06-04 15:07:37 -0400

    Update error handling.

--- listen.py
+++ listen.py
@@ -13,76 +13,83 @@ load_dotenv()
 last_push = time.time()
 
 def log_failure(e):
-    print('Detect failure.')
-    print(time.time())
+    print(f'Detect failure at {time.time()}. Error message:')
     print(e)
 
 def send_data():
-    token = os.environ['GH_TOKEN']
-    owner = "jackvandeleuv"
-    repo = "temp-monitor-data"
-    path = "auto_temp_data.jsonl"
-    api = f"https://api.github.com/repos/{owner}/{repo}/contents/{path}"
-    headers = {
-        "Authorization": f"Bearer {token}",
-        "Accept": "application/vnd.github+json"
-    }
+    try:
+        token = os.environ['GH_TOKEN']
+        owner = "jackvandeleuv"
+        repo = "temp-monitor-data"
+        path = "auto_temp_data.jsonl"
+        api = f"https://api.github.com/repos/{owner}/{repo}/contents/{path}"
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Accept": "application/vnd.github+json"
+        }
 
-    temps = defaultdict(list)
-    humidities = defaultdict(list)
-    with open(f"temp_data_local.jsonl", "rt") as file:
-        for line in file:
-            row = json.loads(line)
-            key = (row['timestamp'] // 1800) * 1800  # Bucket every 30 minutes.
-            temps[key].append(row['temperature'])
-            humidities[key].append(row['humidity'])
+        temps = defaultdict(list)
+        humidities = defaultdict(list)
+        with open(f"temp_data_local.jsonl", "rt") as file:
+            for line in file:
+                row = json.loads(line)
+                key = (row['timestamp'] // 1800) * 1800  # Bucket every 30 minutes.
+                temps[key].append(row['temperature'])
+                humidities[key].append(row['humidity'])
 
-    temp_data = []
-    for timestamp in temps:
-        temp_data.append({
-            'timestamp': timestamp,
-            'temperature': sum(temps[timestamp]) / len(temps[timestamp]),
-            'humidity': sum(humidities[timestamp]) / len(humidities[timestamp])
-        })
+        temp_data = []
+        for timestamp in temps:
+            temp_data.append({
+                'timestamp': timestamp,
+                'temperature': sum(temps[timestamp]) / len(temps[timestamp]),
+                'humidity': sum(humidities[timestamp]) / len(humidities[timestamp])
+            })
 
-    with open("temp_data_local_agg.jsonl", "w") as file:
-        for row in temp_data:
-            file.write(json.dumps(row) + '\n')
-            
-    with open(f"temp_data_local_agg.jsonl", "rb") as file:
-        push(file.read(), api, headers)
+        with open("temp_data_local_agg.jsonl", "w") as file:
+            for row in temp_data:
+                file.write(json.dumps(row) + '\n')
+                
+        with open(f"temp_data_local_agg.jsonl", "rb") as file:
+            push(file.read(), api, headers)
+    except Exception as e:
+        log_failure(e)
 
 def current_sha(api, headers):
-    r = requests.get(api, headers=headers)
-    return r.json()["sha"] if r.status_code == 200 else None
+    try:
+        r = requests.get(api, headers=headers)
+        return r.json()["sha"] if r.status_code == 200 else None
+    except Exception as e:
+        log_failure(e)
 
 def push(blob: bytes, api, headers):
-    body = {
-        "message": f"data {time.strftime('%F %T', time.gmtime())}",
-        "content": base64.b64encode(blob).decode(),
-    }
-    sha = current_sha(api, headers)
-    if sha: body["sha"] = sha
-    r = requests.put(api, headers=headers, data=json.dumps(body))
     try:
+        body = {
+            "message": f"data {time.strftime('%F %T', time.gmtime())}",
+            "content": base64.b64encode(blob).decode(),
+        }
+        sha = current_sha(api, headers)
+        if sha: body["sha"] = sha
+        r = requests.put(api, headers=headers, data=json.dumps(body))
         r.raise_for_status()
     except Exception as e:
-        print(f'\nPush failure at {time.time()}. Error is:')
-        print(e)
+        log_failure(e)
 
 def decode(encoded: bytes):
-    base = int.from_bytes(encoded[2 : 5], "big")
+    try:
+        base = int.from_bytes(encoded[2 : 5], "big")
 
-    if base & 0x800000:
-        base &= 0x7FFFFF 
-        sign = -1
-    else:
-        sign = 1
+        if base & 0x800000:
+            base &= 0x7FFFFF 
+            sign = -1
+        else:
+            sign = 1
 
-    temp = sign * ((base // 1000) / 10.0) 
-    humidity = (base % 1000) / 10.0         
+        temp = sign * ((base // 1000) / 10.0) 
+        humidity = (base % 1000) / 10.0         
 
-    return temp, humidity
+        return temp, humidity
+    except Exception as e:
+        log_failure(e)
 
 
 def detect(device, adv):
